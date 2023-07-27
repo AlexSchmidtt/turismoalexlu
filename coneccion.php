cff83e57e0729d43143bf53aa0a721f7fff0c289
AlexSchmidtt <aschmidt@escuelasproa.edu.ar>
2023-07-27 11:11:51 -0300

    subida

---
+++ coneccion.php
@@ -0,0 +1,8 @@
+$servername= "localhost";
+$database="Tables";
+$username="root";
+$password="proa1234";
+    //creat connection
+$conn=mysqli_connect($localhost,$root,$proa1234,$Tables)
+    //check connection
+    if
